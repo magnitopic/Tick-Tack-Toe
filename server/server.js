0f104721fe974c27b83d464d49f8e32cee0a0038
magnitopic <magnitopic@gmail.com>
2021-11-07 10:39:23 +0100

    Atempt on making a ws page with express and ws

--- server/server.js
+++ server/server.js
@@ -1,7 +1,28 @@
-const WebSocket = require("ws");
+const express = require("express");
+const app=express();
+const server=require('http').createServer(app);
+const WebSocket=require('ws');
+const path = require('path');
 
-const wss = new WebSocket.Server({ port: 8082 });
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "::";
 
-wss.on("connection", (ws) => {
-  console.log("New client");
+// We are using the express server as a server for wss
+const wss = new WebSocket.Server({ server: server });
+
+
+// Renders the page
+app.get('/',(req,res)=>{
+	res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+
+// WSS code
+wss.on('connection',(ws)=> {
+	console.log("New client just connected!");
+});
+
+app.listen(port, host, () =>
+  console.log(`\nServer runing on port ${port} at http://localhost:${port}`)
+);
+
